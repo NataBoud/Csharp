1d7606cdaf60f2ba61519fe9f8db84b0e8c597e3
Natalia Boudard <natalia.boudard@gmail.com>
2026-01-06 10:54:35 +0100

    Ajout d exercice contact mvc

---
+++ ASP.NET MVC/Contacts/Contacts/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
